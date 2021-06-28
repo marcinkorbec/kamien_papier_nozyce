a43e5e7604fc1badd2f8e4a2f9adb15131063168
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-06-28 17:10:24 +0200

    add two objects
    gameSummary and game

--- game.js
+++ game.js
@@ -1,1 +1,22 @@
 // Zobacz gotowy projekt: https://websamuraj.pl/examples/js/projekt7/
+const gameSummary = {
+  numbers: 0,
+  wins: 0,
+  losses: 0,
+  draws: 0
+};
+
+const game = {
+  playerHand: "",
+  aiHand: "",
+  playerHandHTML: ""
+};
+
+const hands = [...document.querySelectorAll('.select img')];
+
+//Pierwsza funkcja
+
+function handSelection() {
+  console.log(this);
+}
+hands.forEach(hand => hand.addEventListener('click', handSelection))
