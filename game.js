bbc6f4ca7b1f3ace2a9109f1aa39ad92ae7afb43
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-06-29 20:36:44 +0200

    update game.js

--- game.js
+++ game.js
@@ -1,15 +1,13 @@
-// Zobacz gotowy projekt: https://websamuraj.pl/examples/js/projekt7/
 const gameSummary = {
   numbers: 0,
   wins: 0,
   losses: 0,
-  draws: 0
+  draws: 0,
 };
 
 const game = {
   playerHand: "",
   aiHand: "",
-  playerHandHTML: ""
 };
 
 const hands = [...document.querySelectorAll('.select img')];
@@ -17,6 +15,66 @@ const hands = [...document.querySelectorAll('.select img')];
 //Pierwsza funkcja
 
 function handSelection() {
-  console.log(this);
+  // console.log(this);
+  game.playerHand = this.dataset.option
+  // console.log(game.playerHand);
+  hands.forEach(hand => hand.style.boxShadow = '');
+  this.style.boxShadow = '0 0 0 4px yellow';
 }
-hands.forEach(hand => hand.addEventListener('click', handSelection))
+
+function aiChoice() {
+  aiHand = hands[Math.floor(Math.random()*3)].dataset.option;
+  return aiHand;
+}
+
+function checkResult(player, ai) {
+  if (player === ai) {
+    return 'draw';
+  } else if ((player === "papier" && ai === "kamień") || (player === "kamień" && ai === "nożyczki") || (player === "nożyczki" && ai === "papier")) {
+    return 'win';
+  } else {
+    return 'loss';
+  }
+}
+
+// Publikacja wyniku
+
+function publishResult(player, ai, result) {
+  document.querySelector('[data-summary="your-choice"]').textContent = player;
+  document.querySelector('[data-summary="ai-choice"]').textContent = ai;
+  document.querySelector('p.numbers span').textContent = ++gameSummary.numbers;
+  if(result === "win") {
+    document.querySelector('p.wins span').textContent = ++gameSummary.wins;
+    document.querySelector('[data-summary="who-win"]').textContent = 'TY wygrałeś!';
+    document.querySelector('[data-summary="who-win"]').style.color = 'green';
+  } else if (result === "loss") {
+    document.querySelector('p.losses span').textContent = ++gameSummary.losses;
+    document.querySelector('[data-summary="who-win"]').textContent = 'Komputer wygrał!';
+    document.querySelector('[data-summary="who-win"]').style.color = 'red';
+  } else {
+    document.querySelector('p.draws span').textContent = ++gameSummary.draws;
+    document.querySelector('[data-summary="who-win"]').textContent = 'Remis!';
+    document.querySelector('[data-summary="who-win"]').style.color = 'gray';
+  }
+}
+function endGame(){
+  document.querySelector('[data-option="' + game.playerHand +'"]').style.boxShadow = "";
+  game.playerHand = "";
+  game.aiHand = "";
+}
+//funkcja sterująca
+
+function startGame() {
+  if (game.playerHand === "") {
+    return alert("wybierz dłoń! co grasz papier kamień czy nożyce");
+  }
+  game.aiHand = aiChoice();
+  const gameResult = checkResult(game.playerHand, game.aiHand);
+  console.log(gameResult);
+  publishResult(game.playerHand, game.aiHand, gameResult)
+  endGame();
+}
+
+hands.forEach(hand => hand.addEventListener('click', handSelection));
+
+document.querySelector('.start').addEventListener('click', startGame);
